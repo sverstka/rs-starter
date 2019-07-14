1ce7cf855576af8d8387fe1e127a86c67e023473
sverstka <sverstka@gmail.com>
2019-07-14 22:00:43 +0300

    fix: stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,45 +1,34 @@
 module.exports = {
-  extends: [
-    'stylelint-config-standard',
-    'stylelint-config-recommended-scss'
-  ],
-  ignoreFiles: [],
-  rules: {
-    'block-no-empty': null,
-    'number-leading-zero': 'never',
-    'declaration-empty-line-before': 'never',
-    'selector-pseudo-element-colon-notation': 'single',
-    'color-no-invalid-hex': true,
-    'declaration-colon-space-after': 'always',
-    'declaration-colon-space-before': 'never',
-    'function-comma-space-after': 'always',
-    'media-feature-colon-space-after': 'always',
-    'media-feature-colon-space-before': 'never',
-    'media-feature-name-no-vendor-prefix': true,
-    'max-empty-lines': 5,
-    'number-no-trailing-zeros': true,
-    'property-no-vendor-prefix': true,
-    'selector-list-comma-space-before': 'never',
-    'selector-list-comma-newline-after': 'always',
-    'comment-whitespace-inside': 'never',
-    'string-quotes': [
-      'single',
-      {
-        message: 'Кавычки должны быть одинарные'
-      }
-    ],
-    'selector-pseudo-class-no-unknown': [
-      true,
-      {ignorePseudoClasses: ['input-placeholder']}
-    ],
-    'value-no-vendor-prefix': true,
-    'scss/at-rule-no-unknown': [
-      true,
-      {
-        'ignoreAtRules': [
-          'elseif'
-        ]
-      }
-    ]
-  }
+	extends: [
+		'stylelint-config-standard',
+		'stylelint-config-css-modules'
+	],
+	plugins: [
+		'stylelint-order'
+	],
+	rules: {
+		'comment-empty-line-before': [
+			'always',
+			{
+				except: ['first-nested'],
+				ignore: [
+					'after-comment',
+					'stylelint-commands'
+				]
+			}
+		],
+		'declaration-empty-line-before': [
+			'always',
+			{
+				except: ['first-nested'],
+				ignore: ['after-declaration']
+			}
+		],
+		'selector-max-specificity': '0,2,0',
+		'order/order': [
+			'custom-properties',
+			'declarations'
+		],
+		'number-leading-zero': 'never'
+	}
 };
