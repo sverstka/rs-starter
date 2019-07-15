0352108e66b67e41d03d9d2e46c1188436d711a9
sverstka <sverstka@gmail.com>
2019-07-15 09:37:40 +0300

    fix: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+const { ignore } = require('./node_modules/@jetbrains/eslint-config/consts');
+
 module.exports = {
 	root: true,
 	env: {
@@ -6,36 +8,13 @@ module.exports = {
 		browser: true,
 		jquery: true
 	},
-	plugins: [],
-	parser: 'babel-eslint',
-	parserOptions: {
-		ecmaVersion: 6,
-		sourceType: 'module',
-		ecmaFeatures: {
-			impliedStrict: true
-		}
-	},
 	extends: [
-		'airbnb-base'
+		'./node_modules/@jetbrains/eslint-config/index.js',
+		'./node_modules/@jetbrains/eslint-config/es6.js',
+		'./node_modules/@jetbrains/eslint-config/node.js'
 	],
 	rules: {
-		'strict': [0, 'global'],
-		'func-names': 0,
-		'global-require': 0,
-		'function-paren-newline': ['error', 'consistent'],
-		'comma-dangle': ['error', 'never'],
-		'prefer-template': 0,
-		'prefer-destructuring': 0,
-		'no-console': 0,
-		'indent': [2, 'tab', {'SwitchCase': 1}],
-		'no-tabs': 0,
-		'no-var': 0,
-		'no-param-reassign': [2, {'props': false}],
-		'no-trailing-spaces': ['error', {'skipBlankLines': true}],
-		'arrow-body-style': 0,
-		'no-plusplus': 0,
-		'import/extensions': 0,
-		'import/no-unresolved': 0,
-		'import/no-extraneous-dependencies': 0
+		'import/no-commonjs': ignore,
+		'dot-location': ['error', 'property']
 	}
 };
